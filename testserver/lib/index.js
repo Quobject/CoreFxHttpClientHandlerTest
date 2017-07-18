ffbe374589a64302aba6c6201d8544db28498882
MatthiasL <matthias_ludwig@yahoo.com>
2017-07-19 06:58:32 +1000

    express post

--- testserver/lib/index.js
+++ testserver/lib/index.js
@@ -1,10 +1,24 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+//
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-app.get('/', (req, res) => {
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.get('/get', (req, res) => {
     res.send('Hello World!');
+});
+// POST http://localhost:8080/api/users
+// parameters sent with 
+app.post('/api/users', function (req, res) {
+    var user_id = req.body.id;
+    var token = req.body.token;
+    var geo = req.body.geo;
+    res.send(user_id + ' ' + token + ' ' + geo);
 });
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
+    console.log('http://localhost:3000/get');
+    console.log('http://localhost:3000/api/users');
 });
